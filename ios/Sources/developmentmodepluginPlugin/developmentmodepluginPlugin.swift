3f5ffe116d2f8ca9247602d020bcc111c69d782d
patzboy <kpatangan@gmail.com>
2025-05-30 16:53:14 +0800

    commit isdevelopment ios()

--- ios/Sources/developmentmodepluginPlugin/developmentmodepluginPlugin.swift
+++ ios/Sources/developmentmodepluginPlugin/developmentmodepluginPlugin.swift
@@ -6,18 +6,35 @@ import Capacitor
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(developmentmodepluginPlugin)
-public class developmentmodepluginPlugin: CAPPlugin, CAPBridgedPlugin {
-    public let identifier = "developmentmodepluginPlugin"
-    public let jsName = "developmentmodeplugin"
-    public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
-    ]
-    private let implementation = developmentmodeplugin()
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+public class developmentmodepluginPlugin: CAPPlugin {
+   @objc func isDeveloperModeEnabled(_ call: CAPPluginCall) {
+        let isDev = isDebuggerAttached() || isRunningInSimulator() || isDebugBuild()
         call.resolve([
-            "value": implementation.echo(value)
+            "developerMode": isDev
         ])
+    }
+
+    private func isDebuggerAttached() -> Bool {
+        var info = kinfo_proc()
+        var size = MemoryLayout<kinfo_proc>.stride
+        var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        let result = sysctl(&name, u_int(name.count), &info, &size, nil, 0)
+        return result == 0 && (info.kp_proc.p_flag & P_TRACED) != 0
+    }
+
+    private func isRunningInSimulator() -> Bool {
+        #if targetEnvironment(simulator)
+        return true
+        #else
+        return false
+        #endif
+    }
+
+    private func isDebugBuild() -> Bool {
+        #if DEBUG
+        return true
+        #else
+        return false
+        #endif
     }
 }
